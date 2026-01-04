1ca45169a9b68a16e0a0e6289771761d69f2c14a
WavingRoger <puranjotsinghpsn@gmail.com>
2026-01-04 22:17:28 +0530

    added file_ignore_patterns in nvim telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,7 +3,16 @@ return {
   tag = "0.1.8",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
   },
-
+  opts = {
+    defaults = {
+      file_ignore_patterns = {
+        "node_modules/",
+        ".git/",
+        "dist/",
+        "build/",
+      },
+    },
+  },
 }
